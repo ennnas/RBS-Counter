ba61a3148607797199b818191ceb1135c21f79a3
Ennio Nasca <ennionasca@iMac-di-Grazia.homenet.telecomitalia.it>
2017-02-09 13:37:50 +0100

    feat: added team logo instead of text

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -4,13 +4,24 @@ import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import java.lang.Math;
+
+import static android.R.attr.id;
+import static android.os.Build.VERSION_CODES.M;
+import static android.provider.ContactsContract.Directory.PACKAGE_NAME;
 
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
     int scoreTeamB = 0;
+
+    int logoA = 0;
+    int logoB = 0;
+
+    String[] nations = {"italy","france","england","ireland","wales","scotland"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +72,26 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamB = 0;
         displayForTeamA(scoreTeamA);
         displayForTeamB(scoreTeamB);
+        ImageView imageviewA = (ImageView) findViewById(R.id.logoTeamA);
+        imageviewA.setImageResource(R.drawable.base);
+        ImageView imageviewB = (ImageView) findViewById(R.id.logoTeamB);
+        imageviewB.setImageResource(R.drawable.base);
+    }
+
+    public void changeLogoA(View view){
+        logoA = Math.abs(logoA + Integer.parseInt(view.getTag().toString()))%6;
+        int id = getResources().getIdentifier("com.example.android.courtcounter:drawable/" + nations[logoA], null, null);
+        System.out.print(id);
+        ImageView imageview = (ImageView) findViewById(R.id.logoTeamA);
+        imageview.setImageResource(id);
+    }
+
+    public void changeLogoB(View view){
+        logoB = Math.abs(logoB + Integer.parseInt(view.getTag().toString()))%6;
+        int id = getResources().getIdentifier("com.example.android.courtcounter:drawable/" + nations[logoB], null, null);
+        System.out.print(id);
+        ImageView imageview = (ImageView) findViewById(R.id.logoTeamB);
+        imageview.setImageResource(id);
     }
 
 }
